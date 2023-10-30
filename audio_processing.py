4868ea1100bb9f643056f3a8c3299122544d949a
radioxeth <radiox@nad27.net>
2023-10-30 11:32:41 -0600

    display app server

--- audio_processing.py
+++ audio_processing.py
@@ -57,20 +57,22 @@ df.head()
 
 current_directory = os.getcwd() + "/"
 
-# Load the audio file
-# meta_data_df = pd.read_csv("metadata.csv", header=None)
-# meta_data_df.head()
-
 file_names=df.columns[0]
 file_names = [file_names] + df[file_names].tolist()
 IMAGE_DIR='images'
-RELATIVE_AUDIO_DIR='target_voice'
-# for dir in ['target_voice','other_voice']:
+APP_IMAGE_DIR='display/images'
 
 if not os.path.exists(IMAGE_DIR) and  not os.path.isdir(IMAGE_DIR):
     os.mkdir(IMAGE_DIR)
+if not os.path.exists(f'{IMAGE_DIR}/target_voice') and  not os.path.isdir(f'{IMAGE_DIR}/target_voice'):
     os.mkdir(f'{IMAGE_DIR}/target_voice')
+if not os.path.exists(f'{IMAGE_DIR}/other_voice') and  not os.path.isdir(f'{IMAGE_DIR}/other_voice'):
     os.mkdir(f'{IMAGE_DIR}/other_voice')
+if not os.path.exists(f'{APP_IMAGE_DIR}/target_voice') and  not os.path.isdir(f'{APP_IMAGE_DIR}/target_voice'):
+    os.mkdir(f'{APP_IMAGE_DIR}/target_voice')
+if not os.path.exists(f'{APP_IMAGE_DIR}/other_voice') and  not os.path.isdir(f'{APP_IMAGE_DIR}/other_voice'):
+    os.mkdir(f'{APP_IMAGE_DIR}/other_voice')
+
 for file_name in file_names:
     try:
         AUDIO_FILE = f'{current_directory}{file_name}'
@@ -92,8 +94,11 @@ for file_name in file_names:
         shift_aud = AudioUtil.time_shift(dur_aud, shift_pct)
         sgram = AudioUtil.spectro_gram(shift_aud, n_mels=64, n_fft=1024, hop_len=None)
         aug_sgram = AudioUtil.spectro_augment(sgram, max_mask_pct=0.1, n_freq_masks=1, n_time_masks=1)
+
         spectrogram_file_name=f"{IMAGE_DIR}/{file_name}.png"
-        plot_spectrogram(np.squeeze(aug_sgram), file_name=spectrogram_file_name)
+        plot_spectrogram(np.squeeze(aug_sgram), title=file_name, file_name=spectrogram_file_name)
+        app_file_name=f"{APP_IMAGE_DIR}/{file_name}.png"
+        plot_spectrogram(np.squeeze(aug_sgram),title=file_name, file_name=app_file_name)
     except Exception as e:
         print(f"An error occured: {e}")
 
